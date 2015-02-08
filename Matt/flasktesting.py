70bc27ac91dd9ce599766413fd284615a90e153d
kynes446 <mattlee446@gmail.com>
2015-02-07 21:52:27 -0500

    everything

--- Matt/flasktesting.py
+++ Matt/flasktesting.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import pymongo
+from pymongo import MongoClient
+
+URI = "mongodb://admin:chalupacity@ds041651.mongolab.com:41651/rufound"
+client = MongoClient(URI)
+db = client.rufound
+collection = db.namelist
+firstname = ""
+middlei = ""
+lastname = ""
 
 app = Flask(__name__)
 
@@ -9,14 +19,24 @@ def my_form():
     return render_template("bananas.html")
 
 @app.route('/', methods=['POST'])
+
 def my_form_post():
+	global firstname
+	global lastname
+	global middlei
+
 	textarray = []
-	string1 = request.form['firstname']
-	#string2 = request.form['lastname']
-   
-	print string1
-	return string1
+	firstname = request.form['firstname']
+	middlei = request.form['middlei']
+	lastname = request.form['lastname']
+	
+	everything = db.nameList.find({firstname + " " + middlei + " " + lastname : 
+		firstname + " " + middlei + " " + lastname } )
+
+	print everything
+	
+	return render_template("bananas.html")
 	
 
 if __name__ == '__main__':
-    app.run()
+	app.run(debug=True)
