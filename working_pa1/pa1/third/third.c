60bf4e8fcaede884d38fc1619332941be56b31e0
frogeyedpeas <sid.ghoshal@yahoo.com>
2015-02-15 15:57:51 -0500

    hi

--- working_pa1/pa1/third/third.c
+++ working_pa1/pa1/third/third.c
@@ -6,37 +6,38 @@
 *****************************/
 
 int true = 1;
-struct lNode //assumed to be in size sorted order
+struct lNode 
 {
 
-	struct lNode *child; //the pointer to the next node
-	struct lNode *prev; //the pointer to the previous node
-	int arg; //the value contained
+	struct lNode *child; 
+	struct lNode *prev; 
+	int arg; 
 
 };
 
 struct lNode *removeNode(struct lNode *head, int value)
 {
-	struct lNode *mediator = head; //a new node pointer for the head so we don't lose the head
+	struct lNode *mediator;
+	mediator = head; 
 
 	while(true)
 	{
-		//printf("%d \n", (*mediator).arg);
-		if(mediator == NULL) //we're at the end of the list. Nothing to delete here
+		
+		if(mediator == NULL) 
 		{
 			return(head);
 		}
-		if((*mediator).arg == value) //we found it and need to delete
+		if((*mediator).arg == value) 
 		{
-															//* -> retrieve object, & -> retrieve address
-			if((*mediator).prev == NULL && (*mediator).child == NULL ) //one piece object
+															
+			if((*mediator).prev == NULL && (*mediator).child == NULL ) 
 			{
-				return(NULL); //thats it!
+				return(NULL); 
 			}
-			if((*mediator).prev != NULL) //Null check for previous
+			if((*mediator).prev != NULL) 
 			{
 
-				(*((*mediator).prev)).child = (*mediator).child; //the previous child is the this's child
+				(*((*mediator).prev)).child = (*mediator).child; 
 
 
 			}
@@ -44,99 +45,100 @@ struct lNode *removeNode(struct lNode *head, int value)
 			
 
 			
-			if((*mediator).child != NULL) //Null check for child
+			if((*mediator).child != NULL) 
 			{
-				(*((*mediator).child)).prev = (*mediator).prev; //the next's previous is this previous
+				(*((*mediator).child)).prev = (*mediator).prev; 
 			}
 			
 			(*mediator).prev = NULL;
 			(*mediator).child = NULL;
-			free(mediator); //frees it
+			free(mediator); 
 
 			
 
 			
 
-			return(head); //returns the head that should now have the modification
+			return(head); 
 		}
 
 		mediator = (*mediator).child;
 	}
 }
 
-void printContents(struct lNode *head) //Prints List Contents
+void printContents(struct lNode *head) 
 {
-	struct lNode *mediator = head;
+	struct lNode *mediator;
+	*mediator = head;
 	
 	while(mediator != NULL)
 	{
-		//printf("%s", "Printing Node Goods \n");
+		
 		printf("%d", (*mediator).arg);
-		mediator = (*mediator).child; //go to the next value
-		if(mediator == NULL) //break out and don't tabify
+		mediator = (*mediator).child; 
+		if(mediator == NULL) 
 		{
 			break;
 		}
-		printf("%c", '\t'); //insert tabs once we know that the next isn't broken
+		printf("%c", '\t'); 
 
 	}
 
-	printf("%c", '\n'); //print the newline
+	printf("%c", '\n'); 
 	return;
 }
 struct lNode *addNode(struct lNode *head, int value)
 {
-	struct lNode *mediator = head; //a new node equivalent to the head pointer
+	struct lNode *mediator;
+	*mediator = head; 
 
-	//struct lNode x; //the node to be added
-	struct lNode *x = malloc(sizeof(struct lNode)); //ALlocation of memory
 	
-	(*x).arg = value; //set its value accordingly
+	struct lNode *x = malloc(sizeof(struct lNode)); 
+	
+	(*x).arg = value; 
 
-	if(mediator == NULL) //If the head itself was a null pointer
+	if(mediator == NULL) 
 	{
 		
-		(*x).prev = NULL; //So that later additions will be simple
-		(*x).child = NULL; //So that later additions will be simple
-		//printf("Initialized with: %d \n", (*x).arg); //REMOVE 
-		return(x); //returns the address
+		(*x).prev = NULL; 
+		(*x).child = NULL; 
+		
+		return(x); 
 	}
 	while(true)
 	{
-		//printf("Value: %d, Address: %p, Child Address: %p, Parent Address: %p: \n",(*mediator).arg, mediator,(*mediator).child, (*mediator).prev);
-		if((*mediator).arg == value) //If its found don't do shit!
+		if((*mediator).arg == value) 
 		{
-			//printf("We already discovered %d \n", x.arg); //REMOVE
+			
 			return head;
 		}
-		if((*mediator).arg > value) //If the current value is too big pass to the previous point
+		if((*mediator).arg > value) 
 		{
-			if((*mediator).prev != NULL) //if the previous value isn't null
+			if((*mediator).prev != NULL) 
 			{
 				
-				(*x).prev = (*mediator).prev; //past of x is the past of the current
-				(*x).child = mediator; //child of x is the current pointer
-				(*mediator).prev = x; //past of the current is x
-				(*((*x).prev)).child = x; //child of the past of x is a pointer to x
-				//printf("%d \n", x.arg); //REMOVE
-				return(head); //return the address or pointer to x
+				(*x).prev = (*mediator).prev; 
+				(*x).child = mediator; 
+				(*mediator).prev = x; 
+				(*((*x).prev)).child = x; 
+				
+				return(head); 
 			}
 
 			
 			(*x).prev = NULL;
 			(*x).child = mediator;
 			(*mediator).prev = x;
-			//printf("%d \n", x.arg); //REMOVE
-			return(x); //returns the pointer to x since x is the head (the head is only element with no previous)
+			
+			return(x); 
 		}
 
-		if((*mediator).child == NULL) //the value isn't bigger since previous code would've applied and the child is NULL!
+		if((*mediator).child == NULL) 
 		{
-			(*x).prev = mediator; //lock down the previous mohterufcker!
+			(*x).prev = mediator; 
 			(*mediator).child = x;
 			(*x).child = NULL;
-			//printf("%s","I have reached the last if! \n"); //REMOVE
-			return(head); //returns the pointer to x
+			
+			return(head); 
 
 		}
 
@@ -152,21 +154,22 @@ int main(int argc, char** argv)
 		return(0);
 	}
 
-	struct lNode *head = NULL; //start off with a null pointer. This gets modified
+	struct lNode *head;
+	head = NULL; 
 	FILE *readfile;
-	int value; //carries the value
-	char type; //determines the type
-	char buffer[100];  //To store character data
+	int value; 
+	char type; 
+	char buffer[100];  
 
-	readfile = fopen(argv[1], "r"); //TIME TO READDDDDD
+	readfile = fopen(argv[1], "r"); 
 
 	if(readfile == NULL)
 	{
-		printf("%s", "error\n"); //Lets us know if file not found
+		printf("%s", "error\n"); 
 		return(0);
 	}
 
-	while(fgets(buffer, 100, readfile) != NULL) //process each line
+	while(fgets(buffer, 100, readfile) != NULL) 
 	{
 		if(sscanf(buffer, "%c %d",&type, &value ) != 2)
 		{
@@ -176,12 +179,9 @@ int main(int argc, char** argv)
 
 		if(type == 'i')
 		{
-			//printf("Adding: %d \n", value);
-			head = addNode(head,value); //add that shit!
-			//printf("Added: %d \n", value);
-			/*printf("%s", "Printing Goods \n");
-			printf("%p \n", (*head).prev); //lets see stuff
-			printf("%p \n", (*head).child); //lets see stuff*/
+			
+			head = addNode(head,value); 
+		
 		}
 		if(type == 'd')
 		{
@@ -189,7 +189,7 @@ int main(int argc, char** argv)
 			head = removeNode(head,value);
 		}
 
-		//printContents(head);
+		
 		if(type != 'i' && type != 'd')
 		{
 			printf("%s", "error\n");
