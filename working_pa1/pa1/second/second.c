fc9b9d2e63beee176b3837d908dac6de529936b7
frogeyedpeas <sid.ghoshal@yahoo.com>
2015-02-15 15:48:44 -0500

    hi

---
+++ working_pa1/pa1/second/second.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+int main(int argc, char** argv)
+{
+	if(argc > 2 || argc < 2)
+	{
+		printf("%s", "error\n");
+		return(0);
+	}
+
+	int x  = atoi(argv[1]);
+	if(x == 1)
+	{
+		printf("%s", "no\n");
+		return(0);
+	}
+	if(x == 2)
+	{
+		printf("%s", "yes\n");
+		return(0);
+	}
+	
+
+	int i = 2; //start it at value 2
+	while(i < x)
+	{
+		if(x%i == 0)
+		{
+			printf("%s", "no\n");
+			return(0);
+		}
+
+		i++;
+	} 
+
+	printf("%s", "yes\n");
+	return(0);
+
+
+}
